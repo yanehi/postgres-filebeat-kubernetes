31ac35f757a18d875e3931dd5d3d73f7eb3a8c30
Yannic Nevado <y.nevado@proventa.de>
2020-04-13 09:49:39 +0100

    change namespace to elk-stack

--- create-deployment.sh
+++ create-deployment.sh
@@ -3,16 +3,15 @@
 #
 set -x
 
-#kubectl create namespace elastic-stack
 echo "Create configmap for Filebeat container"
-kubectl create -f manifests/configmap.yml -n elastic-stack
+kubectl create -f manifests/configmap.yml -n elk-stack
 echo "Serviceaccount for deployment. Read information of pods and namespaces"
-kubectl create -f manifests/serviceaccount.yml -n elastic-stack
+kubectl create -f manifests/serviceaccount.yml -n elk-stack
 echo "Clusterroles for RBAC"
 kubectl create -f manifests/clusterroles.yml  
 echo "Clusterrolebindings for RBAC"
-kubectl create -f manifests/clusterrolebindings.yml -n elastic-stack
+kubectl create -f manifests/clusterrolebindings.yml -n elk-stack
 echo "Persistent volume claim for PostgreSQL logs"
-kubectl create -f manifests/pvc.yml -n elastic-stack
+kubectl create -f manifests/pvc.yml -n elk-stack
 echo "Create Deployment with PostgreSQL container and Filebeat Sidecar"
-kubectl create -f manifests/deployment.yml -n elastic-stack
+kubectl create -f manifests/deployment.yml -n elk-stack
--- remove-deployment.sh
+++ remove-deployment.sh
@@ -5,9 +5,9 @@ set -x
 
 echo "Delete all components of PostgreSQL container with Filebeat sidecar"
 
-kubectl delete deployment postgres -n elastic-stack
-kubectl delete sa filebeat-service-account -n elastic-stack
+kubectl delete deployment postgres -n elk-stack
+kubectl delete sa filebeat-service-account -n elk-stack
 kubectl delete clusterroles filebeat-cluster-role
 kubectl delete clusterrolebinding filebeat-cluster-role-binding
-kubectl delete pvc pg-log -n elastic-stack
-kubectl delete configmaps filebeat-config -n elastic-stack
+kubectl delete pvc pg-log -n elk-stack
+kubectl delete configmaps filebeat-config -n elk-stack
